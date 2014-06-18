b52d743d201929143914f0da59d18fe6a7781b2d
Flaudísio Tolentino <flaudisio@gmail.com>
2014-06-18 16:56:21 -0300

    Torna script de instalação mais robusto
    * Adicionada opção -P/--no-pathogen;
    
    * Opção -f (forçar criação de links) mantida.

--- install.sh
+++ install.sh
@@ -1,13 +1,44 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# install.sh
+#
 
-BaseDir="$( readlink -f "$( dirname "$0" )" )"
+BaseDir="$( cd "$( dirname "$0" )" ; pwd )"
 
-LnOpts="$@"
+LnOpts=""
+Pathogen=1
 
-echo "Criando links..."
+while [[ $# -gt 0 ]] ; do
+    case $1 in
+        -f|--force)
+            LnOpts="-f"
+        ;;
+        -P|--no-pathogen)
+            Pathogen=0
+        ;;
+    esac
+    shift
+done
+
+echo "--> Instalando arquivos..."
 
 for f in "$BaseDir/dotfiles"/* ; do
     ln -sv $LnOpts "$f" "$HOME/.$( basename "$f" )"
 done
 
-echo "Pronto."
+
+if [[ $Pathogen -eq 1 ]] ; then
+    if command -v wget &> /dev/null ; then
+        echo "--> Instalando pathogen.vim..."
+
+        mkdir -pv ~/.vim/{autoload,bundle}
+        wget -q https://tpo.pe/pathogen.vim -O ~/.vim/autoload/pathogen.vim
+
+        sed -i 's/^" execute pathogen/execute pathogen/' "$HOME/.vimrc"
+    else
+        echo "--> wget não encontrado, pathogen.vim não instalado." >&2
+    fi
+fi
+
+
+echo "--> Pronto."
