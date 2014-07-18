d2a9b119fb450a45693aaefdac2d97d369a4a6d2
Flaudísio Tolentino <flaudisio@gmail.com>
2014-07-18 20:35:53 -0300

    Bash: adiciona suporte a diretórios extras
    Agora é possível criar os diretórios ~/.bashrc.d e ~/.profile.d,
    durante a instalação, para que scripts adicionais possam ser
    avaliados sem ser versionados no repositório principal.

--- install.sh
+++ install.sh
@@ -6,15 +6,17 @@
 BaseDir="$( cd "$( dirname "$0" )" ; pwd )"
 
 LnOpts=""
+EnvDirs=1
 Pathogen=1
 
 usage()
 {
     cat << EOF
-Uso: $0 [-f] [-P] [-h]
+Uso: $0 [-f] [-B] [-P] [-h]
 
 Opções:
     -f, --force         Sobrescreve os .arquivos existentes.
+    -D, --no-env-dirs   Não cria os diretórios ~/.bashrc.d e ~/.profile.d
     -P, --no-pathogen   Não instala o pathogen.vim.
     -h, --help          Esta mensagem.
 EOF
@@ -25,11 +27,18 @@ while [[ $# -gt 0 ]] ; do
         -f|--force)
             LnOpts="-f"
         ;;
+        -D|--no-env-dirs)
+            EnvDirs=0
+        ;;
         -P|--no-pathogen)
             Pathogen=0
         ;;
         -h|--help)
             usage ; exit 0
+        ;;
+        *)
+            echo -e "Opção desconhecida: $1\n\n$( usage )" >&2
+            exit 2
         ;;
     esac
     shift
@@ -41,6 +50,10 @@ for f in "$BaseDir/dotfiles"/* ; do
     ln -sv $LnOpts "$f" "$HOME/.$( basename "$f" )"
 done
 
+if [[ $EnvDirs -eq 1 ]] ; then
+    echo "--> Criando diretórios adicionais..."
+    mkdir -pv ~/.bashrc.d ~/.profile.d
+fi
 
 if [[ $Pathogen -eq 1 ]] ; then
     if command -v wget &> /dev/null ; then
